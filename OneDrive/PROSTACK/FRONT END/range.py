67f2ff1ed72528ebf914b2abe2978d7cd602dee9
shireesha-reddy <shireesha047@gmail.com>
2021-02-19 19:24:42 +0530

    first.commit

---
+++ OneDrive/PROSTACK/FRONT END/range.py
@@ -0,0 +1,12 @@
+''' for x in range (1,5):
+    print("A" * 3)
+
+    for x in range (1,10):
+        print("A" * 3) '''
+
+for x in range (5):
+    print("5" * x)
+
+    for x in range (5):
+        print( "   A  "  * x)
+    
